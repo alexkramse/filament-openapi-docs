6aa4b6c491853b54a244cf4f01c64f2e2c19d4ad
Oleksandr Kramarenko <dev.kramarenko@gmail.com>
2026-07-13 23:54:15 +0200

    refactor(openapi-docs): align request sample markup

--- resources/views/components/request-snippet.blade.php
+++ resources/views/components/request-snippet.blade.php
@@ -1,57 +1,64 @@
 @if (! $requestData['hasRequestSamples'])
     <p class="fi-section-header-description">No request sample available.</p>
 @else
-    <div class="foad-request-snippet">
-        <div class="foad-sample-toolbar">
-            @if (count($requestData['requests']) > 1)
+    <div class="fi-sc-component">
+        <div class="fi-sc-tabs fi-contained">
+            <nav class="fi-tabs fi-contained">
+                @if (count($requestData['requests']) > 1)
+                    <x-filament::input.wrapper>
+                        <x-filament::input.select x-model="activeRequest" aria-label="Request example">
+                            @foreach ($requestData['requests'] as $request)
+                                <option value="{{ $request['key'] }}">{{ $request['label'] }}</option>
+                            @endforeach
+                        </x-filament::input.select>
+                    </x-filament::input.wrapper>
+                @endif
+
                 <x-filament::input.wrapper>
-                    <x-filament::input.select x-model="activeRequest" aria-label="Request example">
-                        @foreach ($requestData['requests'] as $request)
-                            <option value="{{ $request['key'] }}">{{ $request['label'] }}</option>
-                        @endforeach
+                    <x-filament::input.select
+                        x-model="activeTarget"
+                        x-on:change="selectTarget()"
+                        aria-label="Request sample language"
+                    >
+                        <template x-for="target in targets" x-bind:key="target.key">
+                            <option
+                                x-bind:value="target.key"
+                                x-bind:selected="target.key === activeTarget"
+                                x-text="target.label"
+                            ></option>
+                        </template>
                     </x-filament::input.select>
                 </x-filament::input.wrapper>
-            @endif
 
-            <x-filament::input.wrapper>
-                <x-filament::input.select
-                    x-model="activeTarget"
-                    x-on:change="selectTarget()"
-                    aria-label="Request sample language"
-                >
-                    <template x-for="target in targets" x-bind:key="target.key">
-                        <option
-                            x-bind:value="target.key"
-                            x-bind:selected="target.key === activeTarget"
-                            x-text="target.label"
-                        ></option>
+                <x-filament::input.wrapper>
+                    <x-filament::input.select x-model="activeClient" aria-label="Request sample client">
+                        <template x-for="client in selectedClients" x-bind:key="client.key">
+                            <option
+                                x-bind:value="client.key"
+                                x-bind:selected="client.key === activeClient"
+                                x-text="client.label"
+                            ></option>
+                        </template>
+                    </x-filament::input.select>
+                </x-filament::input.wrapper>
+
+                <x-filament::button
+                    type="button"
+                    x-on:click="copy()"
+                    x-text="copied ? 'Copied' : 'Copy'"
+                />
+            </nav>
+            <div class="fi-sc-tabs-tab fi-active">
+                <div>
+                    <template x-if="error">
+                        <p class="foad-sample-error" x-text="error"></p>
                     </template>
-                </x-filament::input.select>
-            </x-filament::input.wrapper>
 
-            <x-filament::input.wrapper>
-                <x-filament::input.select x-model="activeClient" aria-label="Request sample client">
-                    <template x-for="client in selectedClients" x-bind:key="client.key">
-                        <option
-                            x-bind:value="client.key"
-                            x-bind:selected="client.key === activeClient"
-                            x-text="client.label"
-                        ></option>
-                    </template>
-                </x-filament::input.select>
-            </x-filament::input.wrapper>
+                    <pre class="foad-sample-code"><code x-bind:class="`language-${prismLanguage}`"
+                                                        x-html="highlightedCode"></code></pre>
+                </div>
+            </div>
 
-            <x-filament::button
-                type="button"
-                x-on:click="copy()"
-                x-text="copied ? 'Copied' : 'Copy'"
-            />
         </div>
-
-        <template x-if="error">
-            <p class="foad-sample-error" x-text="error"></p>
-        </template>
-
-        <pre class="foad-sample-code"><code x-bind:class="`language-${prismLanguage}`" x-html="highlightedCode"></code></pre>
     </div>
 @endif
